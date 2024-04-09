5e88b8db82dc8a898cb10e84ee7f985ab5b678de
doonguy <80924258+doonguy@users.noreply.github.com>
2024-04-09 17:34:03 -0400

    tidy some things up
    - remove Architectury API dependency
    - fix mod metadata
    - greatly increase compatibility
    - remove some unnecessary code

--- forge/src/main/java/doonguy/badhorsefix/forge/BadHorseFixForge.java
+++ forge/src/main/java/doonguy/badhorsefix/forge/BadHorseFixForge.java
@@ -1,15 +1,7 @@
 package doonguy.badhorsefix.forge;
 
-import dev.architectury.platform.forge.EventBuses;
-import doonguy.badhorsefix.BadHorseFix;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(BadHorseFix.MOD_ID)
+@Mod("badhorsefix")
 public class BadHorseFixForge {
-    public BadHorseFixForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(BadHorseFix.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-        BadHorseFix.init();
-    }
 }
